36019f943049f1117eee13740a734d4aa790b11d
Yakubovskaya <maribars93@yandex.ru>
2020-06-12 00:27:51 +0300

    feat: make responsive layout for projects section

--- js/main.js
+++ js/main.js
@@ -41,8 +41,8 @@ $(document).ready(function () {
     var prev = $('.swiper-button-prev');
     var bullets = $('.swiper-pagination');
 
-    next.css('left', prev.width() + 10 + bullets.width() + 10)
-    bullets.css('left', prev.width() + 10)
+    next.css('left', prev.width() + 20 + bullets.width() + 20)
+    bullets.css('left', prev.width() + 20)
 
 });
 
